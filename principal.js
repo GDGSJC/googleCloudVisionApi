f05c60c46dcb3c6c84b67e8801250bfcee66e9d1
Fred <fredmpeixoto@live.com>
2017-12-07 11:37:02 -0200

    api consultando pela url

--- principal.js
+++ principal.js
@@ -1,26 +1,90 @@
 var myApp = angular.module('myApp', []);
 
-
 myApp.controller("fotoController", function ($scope, $http) {
 
-        $scope.enviaPhoto = function (value) {
+  var url = 'https://vision.googleapis.com/v1/images:annotate?key=', key = 'AIzaSyDhsFgqjxXhMI6XFP6bgSmwogGI_e3AjuI';
+  var urlApi = url + key, gdg = {};
+  $scope.gdg =  { tipo : [
+    'TYPE_UNSPECIFIED',
+    'FACE_DETECTION',
+    'LANDMARK_DETECTION',
+    'LOGO_DETECTION',
+    'LABEL_DETECTION',
+    'TEXT_DETECTION',
+    'DOCUMENT_TEXT_DETECTION',
+    'SAFE_SEARCH_DETECTION',
+    'IMAGE_PROPERTIES',
+    'CROP_HINTS',
+    'WEB_DETECTION'
+  ]};
 
-          //  var img = $('#selecionaArquivo');
-            var imagem = document.getElementById('selecionaArquivo');
-       
-           var imagemFile = imagem.files[0];
-            var formData = new FormData();
-            formData.append('imagem', imagemFile);
 
-            
-           console.log(imagemFile);
+  var sendUrl = function (url, type, maxResults) {
+    return sendimageUri = {
+      "requests": [
+        {
+          "image": {
+            "source": {
+              "imageUri": url
+            }
+          },
+          "features": [
+            {
+              "type": type,
+              "maxResults": maxResults
+            }
+          ]
+        }
+      ]
+    }
+  }
 
-           $http.post('upload',formData).then(
-             function (resposta) {
-               console.log(resposta);
-               }
-           )
-            
-        };
+  var sendBase64 = function (base64, type, maxResults) {
+    return sendimageBase64 = {
+      "requests": [
+        {
+          "image": {
+            "content": base64
+          },
+          "features": [
+            {
+              "type": type,
+              "maxResults": maxResults
+            }
+          ]
+        }
+      ]
+    }
+  }
 
-    });
+
+  $scope.enviaPhoto = function () {
+
+    var arquivo = document.getElementById('selecionaArquivo').files[0];
+    var base64 = {
+      binary: ''
+    };
+    var reader = new FileReader();
+
+    reader.readAsBinaryString(arquivo);
+
+    reader.onload = function () {
+      base64.binary = (btoa(reader.result));
+      console.warn("start");
+    };
+
+    console.log(base64.binary);
+    console.log($scope.gdg);
+
+    var enviado = sendUrl($scope.gdg.url, $scope.gdg.valueTipo, 2);
+
+    $http.post(urlApi,
+      enviado, {
+        headers: { 'Content-Type': undefined }
+      }
+    ).then(function (res) { 
+      console.log( res);
+    })
+  };
+
+});
--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+const fileUpload = require('express-fileupload');
 //var bodyParser = require('body-parser');
 var fs = require('fs');
+//var base64 = require('base64js');
+var base64 = require('file-base64');
 
-var app = express();
-
+module.exports = app;
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
+app.use(fileUpload());
 
 
-app.post('/upload', function(req, res) {
-    console.log(req.files); // the uploaded file object
-    fs.readFile(res, function(err, data) {
-        var base64data = new Buffer(data).toString('base64');
-        console.log(base64data);
-     });
-  });
+
+app.post('/upload', function (req, res) {
+  if (!req.files)
+   // return res.status(400).send('No files were uploaded.');
+  var sampleFile = req;
+  console.log(sampleFile);
+  console.log(req);
+
+});
 
 app.listen(3000);
 console.log('App rodando!');
-
-module.exports = app;
