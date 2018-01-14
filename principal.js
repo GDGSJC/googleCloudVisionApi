9ce7db6d890d7f306e4c13bfd4d17d4b3a9fdc7b
Fred <fredmpeixoto@live.com>
2018-01-14 21:42:32 -0200

    versao semi final

--- principal.js
+++ principal.js
@@ -3,9 +3,10 @@ var myApp = angular.module('myApp', []);
 myApp.controller("fotoController", function ($scope, $http) {
 
   var url = 'https://vision.googleapis.com/v1/images:annotate?key=', key = 'AIzaSyDhsFgqjxXhMI6XFP6bgSmwogGI_e3AjuI';
-  var urlApi = url + key, gdg = {};
+  var urlApi = url + key;
+  var gdg = {};
   $scope.gdg = {
-    tipo: [ 
+    tipo: [
       'TYPE_UNSPECIFIED',
       'FACE_DETECTION',
       'LANDMARK_DETECTION',
@@ -18,9 +19,12 @@ myApp.controller("fotoController", function ($scope, $http) {
       'CROP_HINTS',
       'WEB_DETECTION'
     ],
-    urlDisplay : '',
-    base64 : {
-      binary : null
+    urlDisplay: '',
+    altUrl:'',
+    showBase64 : false,
+    showUrl : false,
+    base64: {
+      binary: null
     }
   };
 
@@ -66,10 +70,12 @@ myApp.controller("fotoController", function ($scope, $http) {
   $scope.convertBase64 = function () {
     var arquivo = document.getElementById('selecionaArquivo').files[0];
     var reader = new FileReader();
+   // $scope.gdg.altUrl = arquivo.name;
     reader.readAsBinaryString(arquivo);
-    reader.onload = function () {
+    return reader.onload = function ()  {
       ($scope.gdg.base64.binary = btoa(reader.result));
-    };
+    }
+    $scope.url = '';
   };
 
 
@@ -77,10 +83,14 @@ myApp.controller("fotoController", function ($scope, $http) {
 
     console.log($scope.gdg);
 
-    if ($scope.gdg.url == null || $scope.gdg.url == '' ) {
+    if ($scope.gdg.url == null || $scope.gdg.url == '') {
       var enviado = sendBase64($scope.gdg.base64.binary, $scope.gdg.valueTipo, 2);
+      $scope.gdg.showBase64= true;
+      $scope.gdg.showUrl = false;
     } else {
       var enviado = sendUrl($scope.gdg.url, $scope.gdg.valueTipo, 2);
+      $scope.gdg.showBase64= false;
+      $scope.gdg.showUrl = true;
     }
 
     $http.post(urlApi,
@@ -89,6 +99,8 @@ myApp.controller("fotoController", function ($scope, $http) {
       }
     ).then(function (res) {
       console.log(res.data.responses);
+      var myJson = JSON.stringify(res.data.responses, 0, 6);
+      $scope.retornoJson = " Resultado : "+ myJson ;
     })
   };
 
--- recebeImagem.js
+++
@@ -1,8 +0,0 @@
-app.use(express.bodyParser());
-
-
-app.use(bodyParser.json({limit: "50mb"}));
-
-app.post('/', function(request, response){
-    console.log(request);
-});
